e020af990dce8cde9560026fd0372aa48cc1e7c8
chenzuoli <chenzuoli709@163.com>
2019-09-23 23:51:17 +0800

    log model.

---
+++ models/log.go
@@ -0,0 +1,25 @@
+package models
+
+import (
+	"path/filepath"
+	"strings"
+)
+
+var (
+	NotPV []string = []string{"css", "js", "class", "gif", "jpg", "jpeg", "png", "bmp", "ico", "rss", "xml", "swf"}
+)
+
+const big = 0xFFFFFF
+
+func LogPV(url string) bool {
+	ext := filepath.Ext(url)
+	if ext == "" {
+		return true
+	}
+	for _, v := range NotPV {
+		if v == strings.ToLower(ext) {
+			return false
+		}
+	}
+	return true
+}
